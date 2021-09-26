c8a9005c8d3200ef4b7093ddca4a69151d58b48b
Hayden Huynh <huynhhai323@gmail.com>
2021-09-27 03:52:05 +0700

    Updated master with adjusted develop. Changed production database to pet-home-production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 });
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/dev3801', {
+  .connect('mongodb://127.0.0.1:27017/pet-home-production', {
     useCreateIndex: true,
     useUnifiedTopology: true,
     useNewUrlParser: true,
